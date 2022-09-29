e8122559c8c0726a0874dde525aedf3301df513c
Maciej Bugaj <maciejbugaj@poczta.onet.pl>
2022-09-29 21:34:32 +0200

    few changes

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,8 +32,8 @@ namespace API
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {    
-            services.AddDbContext<DataContext>(options => {options.UseSqlite("Connection string");});     
+        {
+            services.AddDbContext<DataContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));    
             services.AddApplicationServices(_config);
             services.AddControllers();
             services.AddCors();
